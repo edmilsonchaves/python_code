642d3149fa5ba0b30425fd3ad7509fd8fe9ee721
edmilsonchaves <58867496+edmilsonchaves@users.noreply.github.com>
2021-08-07 15:15:35 -0300

    atualizando tabuada

--- tabuada.py
+++ tabuada.py
@@ -5,4 +5,4 @@ tabuada = int(input('Escolha a tabuada que deseja visualizar: '))
 
 print('{} {}'.format('TABUADA DO ', tabuada))
 for n in range(1, 11):
-    print('{} x {:2} = {:2}'.format(tabuada, n, (tabuada * n)))
+    print('{} x {:2} = {:2}'.format(tabuada, n, tabuada * n))
